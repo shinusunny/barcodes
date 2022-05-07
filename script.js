5f46d73acb1d9ac7aa35015ea00d14b926db4226
Shinu Samuel Sunny <shinu.s.sunny@gmail.com>
2022-05-08 01:28:29 +0200

    gs1

--- script.js
+++ script.js
@@ -11,12 +11,145 @@ const barcode = {
     18: 'SSSC',
   },
 
-  two_minus: [0, 2, 4, 6, 8, 9, 1, 3, 5, 7],
-  three_plus: [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
-  five_plus: [0, 5, 1, 6, 2, 7, 3, 8, 4, 9],
-  five_minus: [0, 5, 9, 4, 8, 3, 7, 2, 6, 1],
-  three_add: [0, 7, 4, 1, 8, 5, 2, 9, 6, 3],
-  five_add: [0, 9, 7, 5, 3, 1, 8, 6, 4, 2],
+  twoMinus: [0, 2, 4, 6, 8, 9, 1, 3, 5, 7],
+  threePlus: [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
+  fivePlus: [0, 5, 1, 6, 2, 7, 3, 8, 4, 9],
+  fiveMinus: [0, 5, 9, 4, 8, 3, 7, 2, 6, 1],
+  threeAdd: [0, 7, 4, 1, 8, 5, 2, 9, 6, 3],
+  fiveAdd: [0, 9, 7, 5, 3, 1, 8, 6, 4, 2],
+
+  gs1Reset() {
+    this.gs1Dtls = {};
+  },
+
+  price392(ai, price) {
+    let priceMinor;
+    const re = /^392(\d)/;
+    const found = ai.match(re);
+    if (found) {
+      priceMinor = parseInt((price * 100) / 10 ** found[1]);
+    }
+    return priceMinor;
+  },
+
+  gs1: {
+    '01': {
+      len: 14,
+      FNC1: 0,
+      desc: 'Global Trade Item Number (GTIN)',
+      fun(gtin) {
+        barcode.gs1Dtls.gtin14 = gtin;
+        barcode.gs1Dtls.ean13 = gtin.slice(1, 14);
+      },
+    },
+    15: {
+      len: 6,
+      FNC1: 0,
+      desc: 'Best before date (YYMMDD)',
+      fun(expiry) {
+        barcode.gs1Dtls.expiry = expiry;
+        barcode.gs1Dtls.expiryType = '15';
+      },
+    },
+    16: {
+      len: 6,
+      FNC1: 0,
+      desc: 'Sell by date (YYMMDD)',
+      fun(expiry) {
+        barcode.gs1Dtls.expiry = expiry;
+        barcode.gs1Dtls.expiryType = '16';
+      },
+    },
+    17: {
+      len: 6,
+      FNC1: 0,
+      desc: 'Expiration date (YYMMDD)',
+      fun(expiry) {
+        barcode.gs1Dtls.expiry = expiry;
+        barcode.gs1Dtls.expiryType = '17';
+      },
+    },
+    3920: {
+      len: 6,
+      FNC1: 1,
+      desc: 'Price 0 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3921: {
+      len: 7,
+      FNC1: 1,
+      desc: 'Price 1 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3922: {
+      len: 8,
+      FNC1: 1,
+      desc: 'Price 2 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3923: {
+      len: 9,
+      FNC1: 1,
+      desc: 'Price 3 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3924: {
+      len: 10,
+      FNC1: 1,
+      desc: 'Price 4 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3925: {
+      len: 11,
+      FNC1: 1,
+      desc: 'Price 5 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3926: {
+      len: 12,
+      FNC1: 1,
+      desc: 'Price 6 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3927: {
+      len: 13,
+      FNC1: 1,
+      desc: 'Price 7 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3928: {
+      len: 14,
+      FNC1: 1,
+      desc: 'Price 8 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+    3929: {
+      len: 15,
+      FNC1: 1,
+      desc: 'Price 9 decimal',
+      fun(ai, price) {
+        barcode.gs1Dtls.priceMinor = barcode.price392(ai, price);
+      },
+    },
+  },
 
   cdValidate(ean) {
     let valid = true;
@@ -30,7 +163,7 @@ const barcode = {
     return valid;
   },
 
-  pdValidate(ean) {
+  pcdValidate(ean) {
     let valid = true;
     if (isNaN(ean) || ean <= 0 || ean.length !== 13 || +ean[0] !== 2 || ![1, 2, 3, 4, 7, 8, 9].includes(+ean[1])) {
       const errMsg = Object.entries(this.types)
@@ -44,6 +177,60 @@ const barcode = {
       );
       valid = false;
     }
+    return valid;
+  },
+
+  gs1Validate(ean) {
+    this.gs1Reset();
+    this.gs1.barcode = ean;
+    const eanArr = [...ean];
+    let eanRem = ean;
+    let ai = '';
+    let valid = false;
+    let re, found;
+    for (let i = 0; i < eanArr.length; i++) {
+      ai = ai.concat(eanArr[i]);
+      console.log(i, ai);
+      if (ai in this.gs1) {
+        let gs1Item = this.gs1[ai];
+        let len = gs1Item.len;
+        eanRem = eanArr.slice(i + 1).join('');
+        console.log('remaining', eanRem);
+        console.log('gs1item', gs1Item);
+        if (gs1Item.FNC1) {
+          valid = false;
+          re = new RegExp(String.raw`^(\d{0,${len}})([˜_]?)(.*)?`);
+          console.log(re);
+          found = eanRem.match(re);
+          console.log('found fnc1', found);
+          if (found) {
+            valid = true;
+            i += found[1].length + found[2]?.length;
+            re = /[^\d˜_]/;
+            if (found[3]) console.log('found fnc1 #3', found[3].match(re));
+            if (found[3] && (!found[2] || found[3].match(re))) {
+              valid = false;
+              break;
+            }
+            gs1Item.fun(ai, found[1]);
+          }
+        } else {
+          re = new RegExp(String.raw`^(\d{${len}})(.*)?`);
+          found = eanRem.match(re);
+          console.log('found not fnc1', found);
+          if (found) {
+            valid = true;
+            i += len;
+          } else {
+            valid = false;
+            break;
+          }
+          gs1Item.fun(found[1]);
+        }
+        ai = '';
+      }
+    }
+    console.log(barcode);
     return valid;
   },
 
@@ -63,23 +250,23 @@ const barcode = {
   pcdFourDigitPrice(ean) {
     const digits = [...ean];
     let sum =
-      this.two_minus[digits[8]] + this.two_minus[digits[9]] + this.three_plus[digits[10]] + this.five_minus[digits[11]];
+      this.twoMinus[digits[8]] + this.twoMinus[digits[9]] + this.threePlus[digits[10]] + this.fiveMinus[digits[11]];
 
     sum %= 10;
-    return sum == 0 ? 0 : this.three_add[10 - sum];
+    return sum == 0 ? 0 : this.threeAdd[10 - sum];
   },
 
   pcdFiveDigitPrice(ean) {
     const digits = [...ean];
     let sum =
-      this.five_plus[digits[7]] +
-      this.two_minus[digits[8]] +
-      this.five_minus[digits[9]] +
-      this.five_plus[digits[10]] +
-      this.two_minus[digits[11]];
+      this.fivePlus[digits[7]] +
+      this.twoMinus[digits[8]] +
+      this.fiveMinus[digits[9]] +
+      this.fivePlus[digits[10]] +
+      this.twoMinus[digits[11]];
 
     sum %= 10;
-    return sum == 0 ? 0 : this.five_add[10 - sum];
+    return sum == 0 ? 0 : this.fiveAdd[10 - sum];
   },
 };
 
@@ -111,6 +298,12 @@ const pdRes = document.querySelector('.pd-res');
 const pdEan = document.querySelector('.pd-ean');
 const pdTyp = document.querySelector('.pd-typ');
 const pdCpy = document.getElementById('pd-cpy');
+
+// select elements for price check digit page
+const gs1Btn = document.getElementById('gs1-btn');
+const gs1Inp = document.querySelector('.gs1-inp');
+const gs1Res = document.querySelector('.gs1-res');
+const gs1Dtls = document.querySelector('.gs1-dtls');
 
 // initialize
 const init = function () {
@@ -163,7 +356,7 @@ pdBtn.addEventListener('click', function (e) {
 
   let pdi = pdInp.value;
   let pde = '';
-  if (barcode.pdValidate(pdi)) {
+  if (barcode.pcdValidate(pdi)) {
     let ean = `${pdi.slice(0, pdi.length - 1)}`.padStart(12, '0');
     const prc4 = [3, 4].includes(+ean[1]);
     let pdp = prc4 ? 7 : 6;
@@ -186,4 +379,27 @@ pdInp.addEventListener('keydown', function (e) {
 pdCpy.addEventListener('click', function (e) {
   navigator.clipboard.writeText(pdEan.textContent);
   e.preventDefault();
+});
+
+// functions for validate gs1 section
+gs1Btn.addEventListener('click', function (e) {
+  gs1Res.style.visibility = 'hidden';
+
+  let ean = gs1Inp.value;
+  if (barcode.gs1Validate(ean)) {
+    gs1Dtls.textContent = Object.entries(barcode.gs1Dtls)
+      .map(([k, v]) => `${k} ${v}`)
+      .join('\n');
+    gs1Res.style.visibility = 'visible';
+  } else {
+    alert(`Entered length: ${ean.length}\nError: Invalid GS1 barcode`);
+  }
+  e.preventDefault();
+});
+
+gs1Inp.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    gs1Btn.click();
+    e.preventDefault();
+  }
 });
