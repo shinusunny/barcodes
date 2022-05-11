583b4b9ad836dd6fb2b0acd229458d4328397138
Shinu Samuel Sunny <shinu.s.sunny@gmail.com>
2022-05-11 20:59:31 +0200

    offline

--- sw.js
+++ sw.js
@@ -1,12 +1,45 @@
-const cacheName = 'site-cache-v1';
+const cacheName = 'bh-cache-v1';
 const assetsToCache = ['./index.html', './style.css', './script.js'];
 
 self.addEventListener('install', event => {
+  console.log('[Service Worker] Install');
   self.skipWaiting(); // skip waiting
 
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assetsToCache);
     })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
   );
 });
