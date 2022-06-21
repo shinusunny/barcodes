2b6b10112411ead021ec9d6143e1e717963113ef
Shinu Samuel Sunny <shinu.s.sunny@gmail.com>
2022-06-22 01:09:17 +0200

    implement objects

--- script.js
+++ script.js
@@ -13,26 +13,148 @@ if ('serviceWorker' in navigator) {
 }
 
 // main Barcode object
-const Barcode = {
-  types: {
-    8: 'GTIN-8',
-    12: 'GTIN-12',
-    13: 'GTIN-13',
-    14: 'GTIN-14',
-    17: 'GSIN',
-    18: 'SSSC',
-  },
+class Barcode {
+  constructor(ean) {
+    this.ean = ean;
 
-  twoMinus: [0, 2, 4, 6, 8, 9, 1, 3, 5, 7],
-  threePlus: [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
-  fivePlus: [0, 5, 1, 6, 2, 7, 3, 8, 4, 9],
-  fiveMinus: [0, 5, 9, 4, 8, 3, 7, 2, 6, 1],
-  threeAdd: [0, 7, 4, 1, 8, 5, 2, 9, 6, 3],
-  fiveAdd: [0, 9, 7, 5, 3, 1, 8, 6, 4, 2],
+    this.types = {
+      8: 'GTIN-8',
+      12: 'GTIN-12',
+      13: 'GTIN-13',
+      14: 'GTIN-14',
+      17: 'GSIN',
+      18: 'SSSC',
+    };
 
-  gs1Reset() {
+    this.twoMinus = [0, 2, 4, 6, 8, 9, 1, 3, 5, 7];
+    this.threePlus = [0, 3, 6, 9, 2, 5, 8, 1, 4, 7];
+    this.fivePlus = [0, 5, 1, 6, 2, 7, 3, 8, 4, 9];
+    this.fiveMinus = [0, 5, 9, 4, 8, 3, 7, 2, 6, 1];
+    this.threeAdd = [0, 7, 4, 1, 8, 5, 2, 9, 6, 3];
+    this.fiveAdd = [0, 9, 7, 5, 3, 1, 8, 6, 4, 2];
+
     this.gs1Dtls = {};
-  },
+
+    this.gs1 = {
+      '01': {
+        len: 14,
+        FNC1: 0,
+        desc: 'Global Trade Item Number (GTIN)',
+        fun(gtin) {
+          console.log('01', gtin, this);
+          this.gs1Dtls.gtin14 = gtin;
+          this.gs1Dtls.ean13 = gtin.slice(1, 14);
+        },
+      },
+      15: {
+        len: 6,
+        FNC1: 0,
+        desc: 'Best before date (YYMMDD)',
+        fun(expiry) {
+          this.gs1Dtls.expiry = expiry;
+          this.gs1Dtls.expiryType = '15';
+        },
+      },
+      16: {
+        len: 6,
+        FNC1: 0,
+        desc: 'Sell by date (YYMMDD)',
+        fun(expiry) {
+          this.gs1Dtls.expiry = expiry;
+          this.gs1Dtls.expiryType = '16';
+        },
+      },
+      17: {
+        len: 6,
+        FNC1: 0,
+        desc: 'Expiration date (YYMMDD)',
+        fun(expiry) {
+          this.gs1Dtls.expiry = expiry;
+          this.gs1Dtls.expiryType = '17';
+        },
+      },
+      3920: {
+        len: 6,
+        FNC1: 1,
+        desc: 'Price 0 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3921: {
+        len: 7,
+        FNC1: 1,
+        desc: 'Price 1 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3922: {
+        len: 8,
+        FNC1: 1,
+        desc: 'Price 2 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3923: {
+        len: 9,
+        FNC1: 1,
+        desc: 'Price 3 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3924: {
+        len: 10,
+        FNC1: 1,
+        desc: 'Price 4 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3925: {
+        len: 11,
+        FNC1: 1,
+        desc: 'Price 5 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3926: {
+        len: 12,
+        FNC1: 1,
+        desc: 'Price 6 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3927: {
+        len: 13,
+        FNC1: 1,
+        desc: 'Price 7 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3928: {
+        len: 14,
+        FNC1: 1,
+        desc: 'Price 8 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+      3929: {
+        len: 15,
+        FNC1: 1,
+        desc: 'Price 9 decimal',
+        fun(ai, price) {
+          this.gs1Dtls.priceMinor = this.price392(ai, price);
+        },
+      },
+    };
+  }
 
   price392(ai, price) {
     let priceMinor;
@@ -42,159 +164,46 @@ const Barcode = {
       priceMinor = parseInt((price * 100) / 10 ** found[1]);
     }
     return priceMinor;
-  },
+  }
 
-  gs1: {
-    '01': {
-      len: 14,
-      FNC1: 0,
-      desc: 'Global Trade Item Number (GTIN)',
-      fun(gtin) {
-        Barcode.gs1Dtls.gtin14 = gtin;
-        Barcode.gs1Dtls.ean13 = gtin.slice(1, 14);
-      },
-    },
-    15: {
-      len: 6,
-      FNC1: 0,
-      desc: 'Best before date (YYMMDD)',
-      fun(expiry) {
-        Barcode.gs1Dtls.expiry = expiry;
-        Barcode.gs1Dtls.expiryType = '15';
-      },
-    },
-    16: {
-      len: 6,
-      FNC1: 0,
-      desc: 'Sell by date (YYMMDD)',
-      fun(expiry) {
-        Barcode.gs1Dtls.expiry = expiry;
-        Barcode.gs1Dtls.expiryType = '16';
-      },
-    },
-    17: {
-      len: 6,
-      FNC1: 0,
-      desc: 'Expiration date (YYMMDD)',
-      fun(expiry) {
-        Barcode.gs1Dtls.expiry = expiry;
-        Barcode.gs1Dtls.expiryType = '17';
-      },
-    },
-    3920: {
-      len: 6,
-      FNC1: 1,
-      desc: 'Price 0 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3921: {
-      len: 7,
-      FNC1: 1,
-      desc: 'Price 1 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3922: {
-      len: 8,
-      FNC1: 1,
-      desc: 'Price 2 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3923: {
-      len: 9,
-      FNC1: 1,
-      desc: 'Price 3 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3924: {
-      len: 10,
-      FNC1: 1,
-      desc: 'Price 4 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3925: {
-      len: 11,
-      FNC1: 1,
-      desc: 'Price 5 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3926: {
-      len: 12,
-      FNC1: 1,
-      desc: 'Price 6 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3927: {
-      len: 13,
-      FNC1: 1,
-      desc: 'Price 7 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3928: {
-      len: 14,
-      FNC1: 1,
-      desc: 'Price 8 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-    3929: {
-      len: 15,
-      FNC1: 1,
-      desc: 'Price 9 decimal',
-      fun(ai, price) {
-        Barcode.gs1Dtls.priceMinor = Barcode.price392(ai, price);
-      },
-    },
-  },
-
-  cdValidate(ean) {
+  cdValidate() {
     let valid = true;
-    if (isNaN(ean) || ean <= 0 || !this.types[ean.length + 1]) {
+    console.log(this.ean);
+    if (isNaN(this.ean) || this.ean <= 0 || !this.types[this.ean.length + 1]) {
       const errMsg = Object.entries(this.types)
         .map(([k, v]) => `${k - 1} (${v})`)
         .join(', ');
-      alert(`Entered length: ${ean.length}\nValid lengths: ${errMsg}`);
+      alert(`Entered length: ${this.ean.length}\nValid lengths: ${errMsg}`);
       valid = false;
     }
     return valid;
-  },
+  }
 
-  pcdValidate(ean) {
+  pcdValidate() {
     let valid = true;
-    if (isNaN(ean) || ean <= 0 || ean.length !== 13 || +ean[0] !== 2 || ![1, 2, 3, 4, 7, 8, 9].includes(+ean[1])) {
+    if (
+      isNaN(this.ean) ||
+      this.ean <= 0 ||
+      this.ean.length !== 13 ||
+      +this.ean[0] !== 2 ||
+      ![1, 2, 3, 4, 7, 8, 9].includes(+this.ean[1])
+    ) {
       const errMsg = Object.entries(this.types)
         .filter(([k, _]) => [13].includes(+k))
         .map(([k, v]) => `${k} (${v})`)
         .join(', ');
       alert(
-        `Entered length: ${ean.length}\nValid lengths: ${errMsg}\nOther Validations: starts with 2 followed by 1, 2, 3, 4, 7, 8 or 9`
+        `Entered length: ${this.ean.length}\nValid lengths: ${errMsg}\nOther Validations: starts with 2 followed by 1, 2, 3, 4, 7, 8 or 9`
       );
       valid = false;
     }
     return valid;
-  },
+  }
 
-  gs1Validate(ean) {
-    this.gs1Reset();
-    this.gs1.Barcode = ean;
-    const eanArr = [...ean];
-    let eanRem = ean;
+  gs1Validate() {
+    this.gs1.Barcode = this.ean;
+    const eanArr = [...this.ean];
+    let eanRem = this.ean;
     let ai = '';
     let valid = false;
     let re, found;
@@ -212,7 +221,7 @@ const Barcode = {
           re = new RegExp(String.raw`^(\d{0,${len}})([~_]?)(.*)?`);
           console.log(re);
           found = eanRem.match(re);
-          console.log('found fnc1', found);
+          console.log('found (fnc1)', found);
           if (found) {
             valid = true;
             i += found[1].length + found[2]?.length;
@@ -222,12 +231,12 @@ const Barcode = {
               valid = false;
               break;
             }
-            gs1Item.fun(ai, found[1]);
+            gs1Item.fun.call(this, ai, found[1]);
           }
         } else {
           re = new RegExp(String.raw`^(\d{${len}})(.*)?`);
           found = eanRem.match(re);
-          console.log('found not fnc1', found);
+          console.log('found (not fnc1)', found);
           if (found) {
             valid = true;
             i += len;
@@ -235,17 +244,17 @@ const Barcode = {
             valid = false;
             break;
           }
-          gs1Item.fun(found[1]);
+          gs1Item.fun.call(this, found[1]);
         }
         ai = '';
       }
     }
-    console.log(Barcode);
+    console.log(this.gs1.Barcode);
     return valid;
-  },
+  }
 
-  checkDigit(ean) {
-    const digits = [...ean].reverse();
+  checkDigit() {
+    const digits = [...this.ean].reverse();
     const { even, odd } = digits.reduce(
       (sum, cur, i) => {
         sum[i % 2 == 0 ? 'even' : 'odd'] += +cur;
@@ -255,19 +264,19 @@ const Barcode = {
     );
     let cd = (odd + even * 3) % 10;
     return cd == 0 ? 0 : 10 - cd;
-  },
+  }
 
-  pcdFourDigitPrice(ean) {
-    const digits = [...ean];
+  pcdFourDigitPrice() {
+    const digits = [...this.ean];
     let sum =
       this.twoMinus[digits[8]] + this.twoMinus[digits[9]] + this.threePlus[digits[10]] + this.fiveMinus[digits[11]];
 
     sum %= 10;
     return sum == 0 ? 0 : this.threeAdd[10 - sum];
-  },
+  }
 
-  pcdFiveDigitPrice(ean) {
-    const digits = [...ean];
+  pcdFiveDigitPrice() {
+    const digits = [...this.ean];
     let sum =
       this.fivePlus[digits[7]] +
       this.twoMinus[digits[8]] +
@@ -277,8 +286,8 @@ const Barcode = {
 
     sum %= 10;
     return sum == 0 ? 0 : this.fiveAdd[10 - sum];
-  },
-};
+  }
+}
 
 // select elements for selection page
 const cdMenu = document.querySelector('.section-0');
@@ -341,12 +350,12 @@ cdMenu.addEventListener('click', function (e) {
 cdBtn.addEventListener('click', function (e) {
   cdRes.style.visibility = 'hidden';
 
-  const ean = cdInp.value;
-  if (Barcode.cdValidate(ean)) {
-    const cd = Barcode.checkDigit(ean);
+  const barcode = new Barcode(cdInp.value);
+  if (barcode.cdValidate()) {
+    const cd = barcode.checkDigit();
     cdEan.textContent = '';
-    cdEan.insertAdjacentHTML('beforeend', `${ean}<span class="cd-cd">${cd}</span>`);
-    cdTyp.textContent = `(${Barcode.types[ean.length + 1]})`;
+    cdEan.insertAdjacentHTML('beforeend', `${barcode.ean}<span class="cd-cd">${cd}</span>`);
+    cdTyp.textContent = `(${barcode.types[barcode.ean.length + 1]})`;
     cdRes.style.visibility = 'visible';
   }
   e.preventDefault();
@@ -368,15 +377,17 @@ cdCpy.addEventListener('click', function (e) {
 pdBtn.addEventListener('click', function (e) {
   pdRes.style.visibility = 'hidden';
 
-  let pdi = pdInp.value;
   let pde = '';
-  if (Barcode.pcdValidate(pdi)) {
+  const barcode = new Barcode(pdInp.value);
+  const pdi = barcode.ean;
+  if (barcode.pcdValidate()) {
     let ean = `${pdi.slice(0, pdi.length - 1)}`.padStart(12, '0');
     const prc4 = [3, 4].includes(+ean[1]);
     let pdp = prc4 ? 7 : 6;
-    let pd = prc4 ? Barcode.pcdFourDigitPrice(ean) : Barcode.pcdFiveDigitPrice(ean);
+    let pd = prc4 ? barcode.pcdFourDigitPrice() : barcode.pcdFiveDigitPrice();
     pde = `${ean.slice(0, pdp)}${pd}${ean.slice(pdp + 1)}`;
-    pdEan.textContent = pde.concat(Barcode.checkDigit(pde));
+    barcode.ean = pde;
+    pdEan.textContent = pde.concat(barcode.checkDigit());
     pdTyp.textContent = `${prc4 ? '(4-digit Price Field)' : '(5-digit Price Field)'}`;
     pdRes.style.visibility = 'visible';
   }
@@ -399,14 +410,14 @@ pdCpy.addEventListener('click', function (e) {
 gs1Btn.addEventListener('click', function (e) {
   gs1Res.style.visibility = 'hidden';
 
-  let ean = gs1Inp.value;
-  if (Barcode.gs1Validate(ean)) {
-    gs1Dtls.textContent = Object.entries(Barcode.gs1Dtls)
+  const barcode = new Barcode(gs1Inp.value);
+  if (barcode.gs1Validate()) {
+    gs1Dtls.textContent = Object.entries(barcode.gs1Dtls)
       .map(([k, v]) => `${k} ${v}`)
       .join('\n');
     gs1Res.style.visibility = 'visible';
   } else {
-    alert(`Entered length: ${ean.length}\nError: Invalid GS1 Barcode`);
+    alert(`Entered length: ${barcode.ean.length}\nError: Invalid GS1 Barcode`);
   }
   e.preventDefault();
 });
